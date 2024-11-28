8355cd03ac58e0e9c9efad89b82e642298639776
LucidBreak <chico501@naver.com>
2024-11-28 14:03:47 +0900

    Initial Commit

---
+++ stt/sttApp.swift
@@ -0,0 +1,17 @@
+//
+//  sttApp.swift
+//  stt
+//
+//  Created by 김준서 on 2024/11/28.
+//
+
+import SwiftUI
+
+@main
+struct sttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
